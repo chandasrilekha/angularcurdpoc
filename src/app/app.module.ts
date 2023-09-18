e56a6a7c9f891377ebe90ce0149d7c7a1bccf9c7
Srilekha Chanda <srilekha.chanda@gspann.com>
2023-09-19 00:17:57 +0530

    Angular login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 import { EmpdashboardComponent } from './empdashboard/empdashboard.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpdashboardComponent
+    EmpdashboardComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
